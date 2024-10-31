3590eb4d9374c19467608f83eafc4735b51e4ebc
Avantika Gupta <avantikagupta1729@gmail.com>
2024-10-31 13:24:23 +0530

    Optimal Version

--- Arrays/RotateArrayDPlaces.java
+++ Arrays/RotateArrayDPlaces.java
@@ -1,14 +1,8 @@
 package Arrays;
 import java.util.Scanner;
+import java.util.Arrays;
 public class RotateArrayDPlaces {
-    public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        int n=sc.nextInt();
-        int arr[]=new int[n];
-        for(int i=0;i<n;i++)
-            arr[i]=sc.nextInt();
-        int d = sc.nextInt();
-        d=d%n;
+    static int[] RotateDPlaces(int arr[],int d,int n){
         int temp[]=new int[d];
         for(int i=0;i<d;i++)
             temp[i]=arr[i];
@@ -19,6 +13,33 @@ public class RotateArrayDPlaces {
         for(int tmp=0;i<n;i++,tmp++){
             arr[i]=temp[tmp];
         }
+        return arr;
+    }
+    static int[] RotatePlaces(int arr[],int d,int n){
+        reverse(arr,0,d-1);
+        reverse(arr,d,n-1);
+        reverse(arr,0,n-1);
+        return arr;
+    }
+    private static void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            // Swap the elements
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+            arr[i]=sc.nextInt();
+        int d = sc.nextInt();
+        d=d%n;
+        arr=RotatePlaces(arr,d,n);
         for(int k: arr)
             System.out.print(k+" ");
     }
