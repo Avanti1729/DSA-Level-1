afe912045881de3183452b90c0a00aabd8d97f5b
Avantika Gupta <avantikagupta1729@gmail.com>
2024-11-04 19:11:20 +0530

    Arrays in C++

---
+++ Arrays/MajorityElement.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+int Majority(vector<int>&arr,int n){
+    map<int,int> mpp;
+    for(int i=0;i<n;i++){
+        mpp[arr[i]]++;
+    }
+    for(auto it:mpp){
+        if(it.second>(n/2))
+            return it.first;
+    }
+    return -1;
+}
+int MajorityElement(vector<int>&arr,int n){
+    int count=0,ele;
+    for(int i=0;i<n;i++){
+        if(count==0){
+            count=1;
+            ele=arr[i];
+        }
+        else if(arr[i]==ele)
+            count++;
+        else 
+            count--;
+    }
+    int count1=0;
+    for(int i=0;i<n;i++){
+        if(arr[i]==ele)
+            count1++;
+    }
+    if(count>(n/2))
+        return ele;
+    return -1;
+}
+int main(){
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<"The element which is the majority is "<<MajorityElement(arr,n);
+}
--- Arrays/TwoSum.cpp
+++ Arrays/TwoSum.cpp
@@ -1,12 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
-    int n,num1;
-    cin>>n>>num1;
-    vector<int>arr(n);
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }
+void TwoSumFirst(vector<int>&arr,int n,int num1){
     map<int,int> mpp;
     for(int i=0;i<n;i++){
         int num=arr[i];
@@ -15,5 +9,23 @@ int main(){
             cout<<num<<" "<<target<<endl;
         mpp[num]=i;
     }
-    
+}
+int main(){
+    int n,num1;
+    cin>>n>>num1;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    sort(arr.begin(),arr.end());
+    int i=0,j=n-1;
+    while(i<j){
+        if(arr[i]+arr[j]==num1)
+            cout<<arr[i]<<" "<<arr[j]<<endl;
+        else if(arr[i]+arr[j]>num1)
+            j--;
+        else
+            i++;
+    }
+
 }
